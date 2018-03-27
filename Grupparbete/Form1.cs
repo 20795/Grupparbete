d4034739bf4c979db7adc41862e4c844fa088268
20795 <20795g@gmail.com>
2018-03-27 12:14:31 +0200

    4 grafer

--- Grupparbete/Form1.cs
+++ Grupparbete/Form1.cs
@@ -15,6 +15,10 @@ namespace Grupparbete
         public Form1()
         {
             InitializeComponent();
+            PopulatingGraph data1 = new PopulatingGraph(chart1, "_age_"); //skapar objekt för att populera grafen chart1
+            PopulatingGraph data2 = new PopulatingGraph(chart2, "_job_"); //skapar objekt för att populera grafen chart2
+            PopulatingGraph data3 = new PopulatingGraph(chart3, "_marital_"); //skapar objekt för att populera grafen chart3
+            PopulatingGraph data4 = new PopulatingGraph(chart4, "_education_"); //skapar objekt för att populera grafen chart4
         }
     }
 }
--- Grupparbete/Program.cs
+++ Grupparbete/Program.cs
@@ -18,5 +18,5 @@ namespace Grupparbete
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
-    }
+     }
 }
